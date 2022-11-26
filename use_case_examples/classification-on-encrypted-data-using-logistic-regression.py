720ed2ee3ffa914ec571f58a0809838df8b347cb
Avimanyu Bandyopadhyay <avimanyu786@gmail.com>
2022-11-26 14:07:50 +0530

    Update classification-on-encrypted-data-using-logistic-regression.py

--- use_case_examples/classification-on-encrypted-data-using-logistic-regression.py
+++ use_case_examples/classification-on-encrypted-data-using-logistic-regression.py
@@ -1,4 +1,4 @@
-#Example of classification on encrypted data using logistic regression mentioned at https://docs.zama.ai/concrete-ml
+# Example of classification on encrypted data using logistic regression mentioned at https://docs.zama.ai/concrete-ml
 import numpy
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
